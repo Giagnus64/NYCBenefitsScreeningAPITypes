2f450ead973cd4b76a70609c815d1644ac7c62f0
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-02-28 18:32:24 -0500

    added a namescape and exports

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,20 @@
-declare namespace NYCBSTypes{
+// Type definitions for NYC BENEFITS SCREENING API [https://screeningapidocs.cityofnewyork.us/overview]
+// Project: NYCBSAPIT TYPING
+// Definitions by: Gianfranco Nuschese <[~A URL FOR YOU~]>
 
-    type LivingRentalType = "" | "MarketRate" | "RentControlled" | "FamilyHome" | "Condo" | "NYCHA" | "RentRegulatedHotel" | "Section213" | "LimitedDividendDevelopment" | "MitchellLama" | "RedevelopmentCompany" | "HDFC";
+/*~ This is the module template file. You should rename it to index.d.ts
+ *~ and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
 
 
-    interface HouseholdConfig {
+export as namespace NYCBSTypes{
+
+    export type LivingRentalType = "" | "MarketRate" | "RentControlled" | "FamilyHome" | "Condo" | "NYCHA" | "RentRegulatedHotel" | "Section213" | "LimitedDividendDevelopment" | "MitchellLama" | "RedevelopmentCompany" | "HDFC";
+
+
+    export interface HouseholdConfig {
     cashOnHand: number;
     livingRenting: boolean;
     livingRentalType?: LivingRentalType;
@@ -14,27 +25,27 @@ declare namespace NYCBSTypes{
     livingPreferNotToSay: boolean;
     }
 
-    type HouseholdMemberType = "" | "HeadOfHousehold" | "Child" | "FosterChild" | "StepChild" | "Grandchild" | "Spouse" | "Parent" | "FosterParent"| "StepParent" | "GrandParent" |"SisterBrother"| "StepSisterStepBrother" | "BoyfriendGirlfriend"| "DomesticPartner" | "Unrelated" | "Other";
+    export type HouseholdMemberType = "" | "HeadOfHousehold" | "Child" | "FosterChild" | "StepChild" | "Grandchild" | "Spouse" | "Parent" | "FosterParent"| "StepParent" | "GrandParent" |"SisterBrother"| "StepSisterStepBrother" | "BoyfriendGirlfriend"| "DomesticPartner" | "Unrelated" | "Other";
 
-    type IncomeType = "" | "Wages" | "SelfEmployment" | "Unemployment" | "CashAssistance" | "ChildSupport" | "DisabilityMedicaid" | "SSI" | "SSDependent" | "SSDisability" | "SSSurvivor" | "SSRetirement" | "NYSDisability" | "Veteran" | "Pension" | "DeferredComp" | "WorkersComp" | "Alimony" | "Boarder" | "Gifts" | "Rental" | "Investment";
+    export type IncomeType = "" | "Wages" | "SelfEmployment" | "Unemployment" | "CashAssistance" | "ChildSupport" | "DisabilityMedicaid" | "SSI" | "SSDependent" | "SSDisability" | "SSSurvivor" | "SSRetirement" | "NYSDisability" | "Veteran" | "Pension" | "DeferredComp" | "WorkersComp" | "Alimony" | "Boarder" | "Gifts" | "Rental" | "Investment";
 
-    type IncomeExpenseFrequency = "" | "Weekly" | "BiWeekly" | "Monthly" | "Semimonthly" | "Yearly";
+    export type IncomeExpenseFrequency = "" | "Weekly" | "BiWeekly" | "Monthly" | "Semimonthly" | "Yearly";
 
-    interface IncomeConfig {
+    export interface IncomeConfig {
         amount: number;
         type: IncomeType;
         frequency: IncomeExpenseFrequency;
     }
 
-    type ExpenseType = "" | "ChildCare" | "ChildSupport" | "DependentCare" | "Rent" | "Medical" | "Heating" | "Cooling" | "Mortgage" | "Utilities" | "Telephone" | "InsurancePremiums";
+    export type ExpenseType = "" | "ChildCare" | "ChildSupport" | "DependentCare" | "Rent" | "Medical" | "Heating" | "Cooling" | "Mortgage" | "Utilities" | "Telephone" | "InsurancePremiums";
 
-    interface ExpenseConfig {
+    export interface ExpenseConfig {
         amount: number;
         type: ExpenseType;
         frequency: IncomeExpenseFrequency;
     }
 
-    interface PersonConfig {
+    export interface PersonConfig {
         age: number;
         student: boolean;
         studentFulltime: boolean;
@@ -53,7 +64,7 @@ declare namespace NYCBSTypes{
         expenses?:[ExpenseConfig]
     }
 
-    interface RequestConfig {
+    export interface RequestConfig {
         household: [HouseholdConfig];
         person: [PersonConfig];
         withholdPayload: boolean;
