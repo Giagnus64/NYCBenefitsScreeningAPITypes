2fe75b60a7097e1d9f6ba9b47179e28faee9131b
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-03-02 18:40:25 -0500

    Added proper comments and module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
 // Type definitions for NYC BENEFITS SCREENING API
 // Project: NYC Benefits Screening API [https://screeningapidocs.cityofnewyork.us/overview]
-// Definitions by: Gianfranco Nuschese <[~A URL FOR YOU~]>
+// Definitions by: Gianfranco Nuschese <[https://github.com/Giagnus64]>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+//This file follows both the module and global namespace definition styles in case this 
 
 export type LivingRentalType =
     | ""
@@ -155,3 +158,133 @@ declare global {
         type LivingRentalType = LivingRentalType_;
     }
 }
+
+// declare module NYCBSTypes {
+
+//     type LivingRentalType =
+//         | ""
+//         | "MarketRate"
+//         | "RentControlled"
+//         | "FamilyHome"
+//         | "Condo"
+//         | "NYCHA"
+//         | "RentRegulatedHotel"
+//         | "Section213"
+//         | "LimitedDividendDevelopment"
+//         | "MitchellLama"
+//         | "RedevelopmentCompany"
+//         | "HDFC";
+
+//     interface HouseholdConfig {
+//         cashOnHand: number;
+//         livingRenting: boolean;
+//         livingRentalType?: LivingRentalType;
+//         livingOwner: boolean;
+//         livingStayingWithFriend: boolean;
+//         livingHotel: boolean;
+//         livingShelter: boolean;
+//         livingPreferNotToSay: boolean;
+//     }
+
+//     type HouseholdMemberType =
+//         | ""
+//         | "HeadOfHousehold"
+//         | "Child"
+//         | "FosterChild"
+//         | "StepChild"
+//         | "Grandchild"
+//         | "Spouse"
+//         | "Parent"
+//         | "FosterParent"
+//         | "StepParent"
+//         | "GrandParent"
+//         | "SisterBrother"
+//         | "StepSisterStepBrother"
+//         | "BoyfriendGirlfriend"
+//         | "DomesticPartner"
+//         | "Unrelated"
+//         | "Other";
+
+//     type IncomeType =
+//         | ""
+//         | "Wages"
+//         | "SelfEmployment"
+//         | "Unemployment"
+//         | "CashAssistance"
+//         | "ChildSupport"
+//         | "DisabilityMedicaid"
+//         | "SSI"
+//         | "SSDependent"
+//         | "SSDisability"
+//         | "SSSurvivor"
+//         | "SSRetirement"
+//         | "NYSDisability"
+//         | "Veteran"
+//         | "Pension"
+//         | "DeferredComp"
+//         | "WorkersComp"
+//         | "Alimony"
+//         | "Boarder"
+//         | "Gifts"
+//         | "Rental"
+//         | "Investment";
+
+//     type IncomeExpenseFrequency =
+//         | ""
+//         | "Weekly"
+//         | "BiWeekly"
+//         | "Monthly"
+//         | "Semimonthly"
+//         | "Yearly";
+
+//     interface IncomeConfig {
+//         amount: number;
+//         type: IncomeType;
+//         frequency: IncomeExpenseFrequency;
+//     }
+
+//     type ExpenseType =
+//         | ""
+//         | "ChildCare"
+//         | "ChildSupport"
+//         | "DependentCare"
+//         | "Rent"
+//         | "Medical"
+//         | "Heating"
+//         | "Cooling"
+//         | "Mortgage"
+//         | "Utilities"
+//         | "Telephone"
+//         | "InsurancePremiums";
+
+//     interface ExpenseConfig {
+//         amount: number;
+//         type: ExpenseType;
+//         frequency: IncomeExpenseFrequency;
+//     }
+
+//     interface PersonConfig {
+//         age: number;
+//         student: boolean;
+//         studentFulltime: boolean;
+//         pregnant: boolean;
+//         unemployed: boolean;
+//         unemployedWorkedLast18Months: boolean;
+//         blind: boolean;
+//         disabled: boolean;
+//         veteran: boolean;
+//         benefitsMedicaid: boolean;
+//         benefitsMedicaidDisability: boolean;
+//         householdMemberType: HouseholdMemberType;
+//         livingOwnerOnDeed?: boolean;
+//         livingRentalOnLease?: boolean;
+//         incomes?: [IncomeConfig];
+//         expenses?: [ExpenseConfig];
+//     }
+
+//     interface RequestConfig {
+//         household: [HouseholdConfig];
+//         person: [PersonConfig];
+//         withholdPayload: boolean;
+//     }
+// }
