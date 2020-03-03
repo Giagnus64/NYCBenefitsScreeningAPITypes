847ad7ec8d1822d26e714f04b6173912898cfe23
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-03-03 02:21:13 -0500

    Fixed an issue with some type arrays

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,7 @@
 // Project: NYC Benefits Screening API [https://screeningapidocs.cityofnewyork.us/overview]
 // Definitions by: Gianfranco Nuschese <[https://github.com/Giagnus64]>
 
-
-//This file follows both the module and global namespace definition styles in case this 
+//This file follows both the module and global namespace definition styles in case this
 
 export type LivingRentalType =
     | ""
@@ -122,169 +121,206 @@ export interface PersonConfig {
     householdMemberType: HouseholdMemberType;
     livingOwnerOnDeed?: boolean;
     livingRentalOnLease?: boolean;
-    incomes?: [IncomeConfig];
-    expenses?: [ExpenseConfig];
+    incomes?: IncomeConfig[];
+    expenses?: ExpenseConfig[];
 }
 
 export interface RequestConfig {
-    household: [HouseholdConfig];
-    person: [PersonConfig];
+    household: HouseholdConfig[];
+    person: PersonConfig[];
     withholdPayload: boolean;
 }
+
+export type ResponseSuccess = "SUCCESS" | "FAILURE";
+
+export interface ErrorConfig {
+    message: string;
+    moreInfo: string;
+    code: string;
+    elementPath: string | null;
+}
+
+export interface EligibleProgramConfig {
+    code: string;
+    name: string;
+}
+
+export interface ResponseConfig {
+    type: ResponseSuccess;
+    eligiblePrograms?: [EligibleProgramConfig];
+    errors?: [ErrorConfig];
+}
 // Helpers to allow referencing Big and BigConstructor from inside the global declaration without creating a self reference
-export type RequestConfig_ = RequestConfig;
-export type IncomeConfig_ = IncomeConfig;
-export type IncomeType_ = IncomeType;
-export type ExpenseConfig_ = ExpenseConfig;
-export type ExpenseType_ = ExpenseType;
-export type IncomeExpenseFrequency_ = IncomeExpenseFrequency;
-export type PersonConfig_ = PersonConfig;
-export type HouseholdConfig_ = HouseholdConfig;
-export type HouseholdMemberType_ = HouseholdMemberType;
-export type LivingRentalType_ = LivingRentalType;
+// export type RequestConfig_ = RequestConfig;
+// export type IncomeConfig_ = IncomeConfig;
+// export type IncomeType_ = IncomeType;
+// export type ExpenseConfig_ = ExpenseConfig;
+// export type ExpenseType_ = ExpenseType;
+// export type IncomeExpenseFrequency_ = IncomeExpenseFrequency;
+// export type PersonConfig_ = PersonConfig;
+// export type HouseholdConfig_ = HouseholdConfig;
+// export type HouseholdMemberType_ = HouseholdMemberType;
+// export type LivingRentalType_ = LivingRentalType;
 
+// // declare global {
+// //     namespace NYCBSApi {
+// //         type RequestConfig = RequestConfig_;
+// //         type IncomeConfig = IncomeConfig_;
+// //         type IncomeType = IncomeType_;
+// //         type ExpenseConfig = ExpenseConfig_;
+// //         type ExpenseType = ExpenseType_;
+// //         type IncomeExpenseFrequency = IncomeExpenseFrequency_;
+// //         type PersonConfig = PersonConfig_;
+// //         type HouseholdConfig = HouseholdConfig_;
+// //         type HouseholdMemberType = HouseholdMemberType_;
+// //         type LivingRentalType = LivingRentalType_;
+// //     }
+// // }
 
-// declare global {
-//     namespace NYCBSApi {
-//         type RequestConfig = RequestConfig_;
-//         type IncomeConfig = IncomeConfig_;
-//         type IncomeType = IncomeType_;
-//         type ExpenseConfig = ExpenseConfig_;
-//         type ExpenseType = ExpenseType_;
-//         type IncomeExpenseFrequency = IncomeExpenseFrequency_;
-//         type PersonConfig = PersonConfig_;
-//         type HouseholdConfig = HouseholdConfig_;
-//         type HouseholdMemberType = HouseholdMemberType_;
-//         type LivingRentalType = LivingRentalType_;
+// declare module NYCBSTypes {
+//     type LivingRentalType =
+//         | ""
+//         | "MarketRate"
+//         | "RentControlled"
+//         | "FamilyHome"
+//         | "Condo"
+//         | "NYCHA"
+//         | "RentRegulatedHotel"
+//         | "Section213"
+//         | "LimitedDividendDevelopment"
+//         | "MitchellLama"
+//         | "RedevelopmentCompany"
+//         | "HDFC";
+
+//     interface HouseholdConfig {
+//         cashOnHand: number;
+//         livingRenting: boolean;
+//         livingRentalType?: LivingRentalType;
+//         livingOwner: boolean;
+//         livingStayingWithFriend: boolean;
+//         livingHotel: boolean;
+//         livingShelter: boolean;
+//         livingPreferNotToSay: boolean;
+//     }
+
+//     type HouseholdMemberType =
+//         | ""
+//         | "HeadOfHousehold"
+//         | "Child"
+//         | "FosterChild"
+//         | "StepChild"
+//         | "Grandchild"
+//         | "Spouse"
+//         | "Parent"
+//         | "FosterParent"
+//         | "StepParent"
+//         | "GrandParent"
+//         | "SisterBrother"
+//         | "StepSisterStepBrother"
+//         | "BoyfriendGirlfriend"
+//         | "DomesticPartner"
+//         | "Unrelated"
+//         | "Other";
+
+//     type IncomeType =
+//         | ""
+//         | "Wages"
+//         | "SelfEmployment"
+//         | "Unemployment"
+//         | "CashAssistance"
+//         | "ChildSupport"
+//         | "DisabilityMedicaid"
+//         | "SSI"
+//         | "SSDependent"
+//         | "SSDisability"
+//         | "SSSurvivor"
+//         | "SSRetirement"
+//         | "NYSDisability"
+//         | "Veteran"
+//         | "Pension"
+//         | "DeferredComp"
+//         | "WorkersComp"
+//         | "Alimony"
+//         | "Boarder"
+//         | "Gifts"
+//         | "Rental"
+//         | "Investment";
+
+//     type IncomeExpenseFrequency =
+//         | ""
+//         | "Weekly"
+//         | "BiWeekly"
+//         | "Monthly"
+//         | "Semimonthly"
+//         | "Yearly";
+
+//     interface IncomeConfig {
+//         amount: number;
+//         type: IncomeType;
+//         frequency: IncomeExpenseFrequency;
+//     }
+
+//     type ExpenseType =
+//         | ""
+//         | "ChildCare"
+//         | "ChildSupport"
+//         | "DependentCare"
+//         | "Rent"
+//         | "Medical"
+//         | "Heating"
+//         | "Cooling"
+//         | "Mortgage"
+//         | "Utilities"
+//         | "Telephone"
+//         | "InsurancePremiums";
+
+//     interface ExpenseConfig {
+//         amount: number;
+//         type: ExpenseType;
+//         frequency: IncomeExpenseFrequency;
+//     }
+
+//     interface PersonConfig {
+//         age: number;
+//         student: boolean;
+//         studentFulltime: boolean;
+//         pregnant: boolean;
+//         unemployed: boolean;
+//         unemployedWorkedLast18Months: boolean;
+//         blind: boolean;
+//         disabled: boolean;
+//         veteran: boolean;
+//         benefitsMedicaid: boolean;
+//         benefitsMedicaidDisability: boolean;
+//         householdMemberType: HouseholdMemberType;
+//         livingOwnerOnDeed?: boolean;
+//         livingRentalOnLease?: boolean;
+//         incomes?: [IncomeConfig];
+//         expenses?: [ExpenseConfig];
+//     }
+
+//     interface RequestConfig {
+//         household: [HouseholdConfig];
+//         person: [PersonConfig];
+//         withholdPayload: boolean;
+//     }
+//     type ResponseSuccess = "SUCCESS" | "FAILURE";
+
+//     interface ErrorConfig {
+//         message: string;
+//         moreInfo: string;
+//         code: string;
+//         elementPath: string | null;
+//     }
+
+//     interface EligibleProgramConfig {
+//         code: string;
+//         name: string;
+//     }
+
+//     interface ResponseConfig {
+//         type: ResponseSuccess;
+//         eligiblePrograms?: [EligibleProgramConfig];
+//         errors?: [ErrorConfig];
 //     }
 // }
-
-declare module NYCBSTypes {
-
-    type LivingRentalType =
-        | ""
-        | "MarketRate"
-        | "RentControlled"
-        | "FamilyHome"
-        | "Condo"
-        | "NYCHA"
-        | "RentRegulatedHotel"
-        | "Section213"
-        | "LimitedDividendDevelopment"
-        | "MitchellLama"
-        | "RedevelopmentCompany"
-        | "HDFC";
-
-    interface HouseholdConfig {
-        cashOnHand: number;
-        livingRenting: boolean;
-        livingRentalType?: LivingRentalType;
-        livingOwner: boolean;
-        livingStayingWithFriend: boolean;
-        livingHotel: boolean;
-        livingShelter: boolean;
-        livingPreferNotToSay: boolean;
-    }
-
-    type HouseholdMemberType =
-        | ""
-        | "HeadOfHousehold"
-        | "Child"
-        | "FosterChild"
-        | "StepChild"
-        | "Grandchild"
-        | "Spouse"
-        | "Parent"
-        | "FosterParent"
-        | "StepParent"
-        | "GrandParent"
-        | "SisterBrother"
-        | "StepSisterStepBrother"
-        | "BoyfriendGirlfriend"
-        | "DomesticPartner"
-        | "Unrelated"
-        | "Other";
-
-    type IncomeType =
-        | ""
-        | "Wages"
-        | "SelfEmployment"
-        | "Unemployment"
-        | "CashAssistance"
-        | "ChildSupport"
-        | "DisabilityMedicaid"
-        | "SSI"
-        | "SSDependent"
-        | "SSDisability"
-        | "SSSurvivor"
-        | "SSRetirement"
-        | "NYSDisability"
-        | "Veteran"
-        | "Pension"
-        | "DeferredComp"
-        | "WorkersComp"
-        | "Alimony"
-        | "Boarder"
-        | "Gifts"
-        | "Rental"
-        | "Investment";
-
-    type IncomeExpenseFrequency =
-        | ""
-        | "Weekly"
-        | "BiWeekly"
-        | "Monthly"
-        | "Semimonthly"
-        | "Yearly";
-
-    interface IncomeConfig {
-        amount: number;
-        type: IncomeType;
-        frequency: IncomeExpenseFrequency;
-    }
-
-    type ExpenseType =
-        | ""
-        | "ChildCare"
-        | "ChildSupport"
-        | "DependentCare"
-        | "Rent"
-        | "Medical"
-        | "Heating"
-        | "Cooling"
-        | "Mortgage"
-        | "Utilities"
-        | "Telephone"
-        | "InsurancePremiums";
-
-    interface ExpenseConfig {
-        amount: number;
-        type: ExpenseType;
-        frequency: IncomeExpenseFrequency;
-    }
-
-    interface PersonConfig {
-        age: number;
-        student: boolean;
-        studentFulltime: boolean;
-        pregnant: boolean;
-        unemployed: boolean;
-        unemployedWorkedLast18Months: boolean;
-        blind: boolean;
-        disabled: boolean;
-        veteran: boolean;
-        benefitsMedicaid: boolean;
-        benefitsMedicaidDisability: boolean;
-        householdMemberType: HouseholdMemberType;
-        livingOwnerOnDeed?: boolean;
-        livingRentalOnLease?: boolean;
-        incomes?: [IncomeConfig];
-        expenses?: [ExpenseConfig];
-    }
-
-    interface RequestConfig {
-        household: [HouseholdConfig];
-        person: [PersonConfig];
-        withholdPayload: boolean;
-  
-}
